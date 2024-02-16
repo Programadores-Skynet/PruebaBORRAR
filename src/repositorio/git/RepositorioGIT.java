01ef93090e6e48c24451f0b2a123e919753a9a24
Fernando Delgado Cruz <ferd9800@gmail.com>
2024-02-16 11:01:19 -0600

    Cambio en este sistema

--- src/repositorio/git/RepositorioGIT.java
+++ src/repositorio/git/RepositorioGIT.java
@@ -15,6 +15,7 @@ public class RepositorioGIT {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hola Como estas");
+        System.out.print("Buen dia");
     }
     
 }
